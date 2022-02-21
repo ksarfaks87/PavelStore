2a9268571f4282574bcd45777eb58501e249a0a9
ksarfaks87 <ksarfaks87@gmail.com>
2022-02-21 20:52:27 +0300

    burger add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import menu from "../img/menu.svg";
+import close from "../img/close.svg";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
       <div className="container">
         <div className="header-content">
           <div>Здесь будет Лого</div>
-          <div>Здесь будет Меню</div>
+          <div className="menu-info">Здесь будет Меню</div>
+          <div className="burger" onClick={showMenu}>
+            {isOpen ? <img src={close} alt="" /> : <img src={menu} alt="" />}
+            {isOpen ? (
+              <div className="burger-menu">Здесь будет Меню для мобилки</div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </div>
